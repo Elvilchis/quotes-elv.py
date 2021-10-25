13622124c9341a16a297f2e8462708d05f84e009
Elvilchis <91902844+Elvilchis@users.noreply.github.com>
2021-10-24 23:13:55 -0500

    Create elv.py

---
+++ elv.py
@@ -0,0 +1,10 @@
+# Emilio Vilchis
+# Introduction to computer science 
+
+quotes = {
+    "Moe": "A wise guy, huh?",
+    "Larry": "Ow!",
+    "Curly": "Nyuk nyuk!",
+    }
+stooge = "Moe"
+print(stooge, "says:", quotes[stooge])
